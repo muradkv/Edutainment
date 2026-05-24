c0df8e6834129c47e22d22f93993d1d75b54dcdc
murad <muradkv@gmail.com>
2026-05-24 21:33:14 +0300

    Initial commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by murad on 24.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
